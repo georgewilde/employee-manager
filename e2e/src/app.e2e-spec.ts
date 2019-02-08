b148249c61ee0e1965eb7d49bff318583c8e0133
George Wilde <george@wildecreations.co.uk>
2019-02-08 10:23:14 +0000

    Change occurances of "Employee viewer" to be "Employee manager"

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to employee-viewer!');
+    expect(page.getTitleText()).toEqual('Welcome to employee-manager');
   });
 
   afterEach(async () => {
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/employee-viewer'),
+      dir: require('path').join(__dirname, '../coverage/employee-manager'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
